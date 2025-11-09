2ce0e2aa079cbbba0c4902f02326d4e48112865e
simone <simonesamardzhiev@gmail.com>
2025-11-09 20:52:24 +0200

    Initial Commit

---
+++ AdminPanel/AdminPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdminPanelApp.swift
+//  AdminPanel
+//
+//  Created by Simone Samardzhiev on 9.11.25.
+//
+
+import SwiftUI
+
+@main
+struct AdminPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdminPanel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdminPanel
+//
+//  Created by Simone Samardzhiev on 9.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
